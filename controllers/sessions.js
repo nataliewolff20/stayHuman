aa1691efee7ca8e3abf8a6fb212027cd30df084a
Natalie Wolff <natalielynnwolff@gmail.com>
2018-05-10 14:20:41 -0400

    login/register functional

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -3,33 +3,24 @@ const router = express.Router();
 const User = require('../models/users.js');
 const bcrypt = require('bcrypt');
 
-// router.get('/new', (req, res)=>{
-//     res.render('sessions/new.ejs');
-// });
+router.get('/new', (req, res)=>{
+    res.render('sessions/new.ejs');
+});
 
 router.post('/', (req, res)=>{
     User.findOne({ username:req.body.username}, (err, foundUser)=>{
         if( bcrypt.compareSync(req.body.password, foundUser.password) ){
             req.session.currentuser = foundUser;
-            res.status(201).json({
-              status:201,
-              message: foundUser
-            })
+            res.redirect('/');
         } else {
-            res.status(401).json({
-              status: 401,
-              message: "Login Failed"
-            })
+            res.send('oops, wrong password')
         }
     });
 });
 
 router.delete('/', (req, res)=>{
     req.session.destroy(()=>{
-      res.status(200).json({
-        status: 200,
-        message: 'Logout Complete'
-      })
+      res.redirect('/');
     })
 })
 
